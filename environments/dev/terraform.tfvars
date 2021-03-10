175c7ac7aa4596a74d1b2dceaf6001f3a36dcd45
Paul Gould <paul.gould@lloydsbanking.com>
2021-03-10 11:46:00 +0000

    Update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="dcsgva-lab-sandbox-paul-gould"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "dcsgva-lab-sandbox-paul-gould-tfstate"
     prefix = "env/prod"
   }
 }
